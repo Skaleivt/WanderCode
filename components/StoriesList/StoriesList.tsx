a5e58ab40385a5e050f674d7904c4035e92ba14c
voueva <voueva.e.v@gmail.com>
2025-11-16 19:29:23 +0100

    StoriesList

--- components/StoriesList/StoriesList.tsx
+++ components/StoriesList/StoriesList.tsx
@@ -1,29 +1,15 @@
-// import {
-//     useState
-// } from 'react';
+'use client';
+
+import Image from "next/image";
 
 import css from './StoriesList.module.css';
-
-// import {
-//     useMutation,
-//     useQueryClient
-// } from '@tanstack/react-query';
-
-// import toast from 'react-hot-toast';
-
-// import Link from 'next/link';
-
 
 interface StoriesListProps {
     stories: Array<any>; // TODO: any -> Story
 }
 
 export default function StoriesList({ stories }: StoriesListProps) {
-    // const [deletingId,
-    // setDeletingId]=useState<string | null>(null);
-    // const queryClient=useQueryClient();
-
-    function getDateString(date: Date): string {
+    const getDateString = (date: Date): string => {
         const day = String(date.getDate()).padStart(2, "0");
         const month = String(date.getMonth() + 1).padStart(2, "0");
         const year = date.getFullYear();
@@ -31,88 +17,51 @@ export default function StoriesList({ stories }: StoriesListProps) {
         return `${day}.${month}.${year}`;
     }
 
+    const viewStory = (id: string): void => {
+        
+    };
+
+    const toogleFavoriteStory = (id: string): void => {
+        
+    };
     
     return <>
         <h2>Популярні історії</h2> 
         <ul className={css.list}>
             {stories.map(item => <li className={css.listItem} key={item._id}>
                 <img className={css.img} src={item.img} alt={item.title} />
+                
                 <div className={css.content}>
-                    <div className={css.category}>{item.category}</div>
-                    <h3 className={css.title}>{item.title}</h3>
-                    <p className={css.description}>{item.article}</p>
-                    <div>{item.ownerId}</div>
-                    <div>{getDateString(new Date(item.date))}</div>
-                    <div>{item.favoriteCount}</div>
+                    <div className={css.info}>
+                        <div className={css.category}>{item.category}</div>
+                        <h3 className={css.title}>{item.title}</h3>
+                        <p className={css.description}>{item.article}</p>
+                    </div>
+
+                    <div className={css.owner}>
+                        <img className={css.avatar} src={item.img} alt={item.ownerId} />
+                        <div className={css.ownerInfo}>
+                            <div>{item.ownerId}</div>
+                            <div>{getDateString(new Date(item.date))} &#8226; {item.favoriteCount} <Image src="/favorite.svg" width={12} height={12} alt="Favorite" /></div>
+                        </div>
+                    </div>
+                    
+                    <div className={css.buttonPanel}>
+                        <button 
+                            onClick={() => viewStory(item._id)}
+                            className={css.button} 
+                        >
+                            Переглянути статтю
+                        </button>
+                        <button 
+                            onClick={() => toogleFavoriteStory(item._id)}
+                            className={[css.button, css.favoriteButton].join(" ")}
+                        >
+                            <Image src="/favorite.svg" width={20} height={20} alt="Favorite" />
+                        </button>
+                    </div>
                 </div>
             </li>)}
         </ul>
     </>;
-
-
-    // return (<ul className= {css.list}> {
-    //         // notes.map(item=> (<li key= {
-    //         //             item.id
-    //         //         }
-
-    //         //         className= {
-    //         //             css.listItem
-    //         //         }
-
-    //         //         > <h2 className= {
-    //         //             css.title
-    //         //         }
-
-    //         //         > {
-    //         //             item.title
-    //         //         }
-
-    //         //         </h2> <p className= {
-    //         //             css.content
-    //         //         }
-
-    //         //         > {
-    //         //             item.content
-    //         //         }
-
-    //         //         </p> <div className= {
-    //         //             css.footer
-    //         //         }
-
-    //         //         > <span className= {
-    //         //             css.tag
-    //         //         }
-
-    //         //         > {
-    //         //             item.tag
-    //         //         }
-
-    //         //         </span> <Link href= {
-    //         //             `/notes/$ {
-    //         //                 item.id
-    //         //             }
-
-    //         //             `
-    //         //         }
-
-    //         //         >View details</Link> <button className= {
-    //         //             css.button
-    //         //         }
-
-    //         //         onClick= {
-    //         //             ()=> handleDelete(item.id)
-    //         //         }
-
-    //         //         disabled= {
-    //         //             deletingId===item.id
-    //         //         }
-
-    //         //         > {
-    //         //             deletingId===item.id ? 'Deleting...' : 'Delete'
-    //         //         }
-
-    //         //         </button> </div> </li>))
-    //         // }
-
-    //     }</ul>);
 }
