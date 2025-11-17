2a2e218cca0e7775c18da1cf85c3460a981f2cc6
OL-surgeon <dasti888@ukr.net>
2025-11-17 20:45:13 +0200

    new

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,8 +6,8 @@ import { FiMenu, FiLogOut } from 'react-icons/fi';
 import Link from 'next/link';
 import styles from './Header.module.css';
 import { MobileMenu } from './MobileMenu';
-// import LoginForm from "..//../app/auth/[authType]/LoginForm";
-// import RegistrationForm from "../forms/RegistrationForm";
+// import LoginForm from '../forms/LoginForm';
+// import RegistrationForm from '../forms/RegistrationForm';
 // import { Modal } from "../modal/Modal";
 // import { ConfirmModal } from "./ConfirmModal";
 
@@ -64,62 +64,11 @@ export const Header = ({
             height="22"
             aria-hidden="true"
           >
-            <use href="/public/symbol-defs.svg#icon-logo" />
+            <use href="/symbol-defs.svg#icon-logo" />
           </svg>
           Подорожники
         </Link>
 
-        <nav className={styles.navDesktop}>
-          <Link href="/">Головна</Link>
-          <Link href="/stories">Історії</Link>
-          <Link href="/travelers">Мандрівники</Link>
-
-          {isAuthenticated && (
-            <>
-              <Link href="/profile">Мій Профіль</Link>
-              <button className={styles.publishBtn}>
-                Опублікувати історію
-              </button>
-            </>
-          )}
-
-          <div className={styles.authButtons}>
-            {!isAuthenticated ? (
-              <>
-                <button
-                  className={styles.loginBtn}
-                  onClick={() => setIsLoginOpen(true)}
-                >
-                  Вхід
-                </button>
-                <button
-                  className={styles.registerBtn}
-                  onClick={() => setIsRegisterOpen(true)}
-                >
-                  Реєстрація
-                </button>
-              </>
-            ) : (
-              <>
-                <div className={styles.userInfo}>
-                  {userAvatar && (
-                    <img
-                      src={userAvatar}
-                      alt="avatar"
-                      className={styles.avatar}
-                    />
-                  )}
-                  <span>{userName}</span>
-                </div>
-                <button
-                  className={styles.logoutBtn}
-                  onClick={handleLogout}
-                  title="Вихід"
-                >
-                  <FiLogOut size={20} />
-                </button>
-              </>
-            )}
         <nav className={styles.navDesktop}>
           <Link href="/">Головна</Link>
           <Link href="/stories">Історії</Link>
